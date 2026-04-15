945ffff9fd25ce108c01fca03a861fbbc2587515
Voron03 <a20091999i@gmail.com>
2026-04-15 15:40:58 +0200

    qdazeaze

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -48,11 +48,13 @@ export async function updateSession(request: NextRequest) {
   const user = data?.claims;
 
   if (
-    request.nextUrl.pathname !== "/" &&
     !user &&
+    request.nextUrl.pathname !== "/" &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/exercices")
   ) {
+
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
